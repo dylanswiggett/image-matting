6d42be0effd0cd12f2f18a4b545b009f6d5eaa2c
Dylan Swiggett <dylanswiggett@gmail.com>
2014-06-15 22:33:54 -0700

    Allows specifying files as command line arguments.

--- impl/main.cpp
+++ impl/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include "GridSampler.hpp"
 #include "ImageManager.hpp"
 #include "FGBGMatte.hpp"
@@ -11,13 +12,23 @@ using namespace Eigen;
 int main(int argc, char** argv) {
   SDL_Init(SDL_INIT_EVERYTHING);
 
-  ImageManager img("test_img/pooh_med.bmp");
-  ImageManager img_guess("test_img/pooh_med_guess.bmp");
+  std::string image_path = "test_img/toast.bmp";
+  std::string img_guess_path = "test_img/toast_guess.bmp";
+  std::string img_out_path = "test_img/toast_result.bmp";
+
+  if (argc == 4) {
+    image_path = argv[1];
+    img_guess_path = argv[2];
+    img_out_path = argv[3];
+  }
+
+  ImageManager img(image_path);
+  ImageManager img_guess(img_guess_path);
 
   FGBGMatte matte(&img, &img_guess);
 
   ImageManager matte_img(matte.GetMatte());
 
-  matte_img.SaveTo("test_img/pooh_med_result.bmp");
+  matte_img.SaveTo(img_out_path);
   return 0;
 }
